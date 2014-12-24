a087151cb175e5cd91f07749896188c58c290273
Richard Remer <richard.remer@searchoptics.com>
2014-12-24 10:14:40 -0800

    wip

---
+++ bonded.js
@@ -0,0 +1,39 @@
+var Promise = require("es6-promise").Promise;
+
+/**
+ * Wrap an asynchronous call to return a Promise synchronously.  If an object
+ * is provided, it will be bound to "this" in the async function.
+ * @param {function} async
+ * @param {object} [obj]
+ * @returns {function}
+ */
+function maybe(async, obj) {
+    // resulting wrapper; accepts one less argument than original async call
+    return function() {
+        // wrapper arguments
+        var args = Array.prototype.slice.call(arguments);
+
+        // return a Promise which will be fulfilled by the async call
+        return new Promise(function(resolve, reject) {
+            // add missing callback argument to args
+            args.push(function(err, result) {
+                // reject on error
+                if (err) reject(err);
+
+                // or resolve with result
+                else resolve(result);
+            });
+
+            // make async call
+            async.apply(obj, args);
+        });
+    };
+}
+
+
+/** export functions */
+module.exports = {
+    maybe: maybe,
+    eventually: eventually,
+    Promise: Promise
+};
---
+++ test/test-bonded.js
@@ -0,0 +1,47 @@
+var bonded = require(".."),
+    expect = require("expect.js"),
+    Promise = bonded.Promise;
+
+// async call which does not generate errors
+function async(arg, done) {
+    setTimeout(function() {done(arg);}, 0);
+}
+
+// async call which will be successful
+function asyncFulfill(arg, done) {
+    setTimeout(function() {done(null, arg);}, 0);
+}
+
+// async call which will fail
+function asyncReject(arg, done) {
+    setTimeout(function() {done(new Error("error"));}, 0);
+}
+
+describe("bonded.maybe", function() {
+    it("should wrap an async call to return Promise", function(done) {
+        var fulfill, reject;
+
+        fulfill = bonded.maybe(asyncFulfill)(42);
+        fulfill.then(function(arg) {
+            expect(arg).to.be(42);
+
+            reject = bonded.maybe(asyncReject)(42);
+            reject.catch(function(err) {
+                expect(err).to.be.an(Error);
+                done();
+            });
+        });
+    });
+});
+
+describe("bonded.eventually", function() {
+    it("should wrap async receiver to return a Promise", function(done) {
+        var eventually = bonded.eventually(async),
+            resultEventually = eventually(42);
+        
+        resultEventually.then(function(result) {
+            expect(result).to.be(42);
+            done();
+        });
+    });
+});
